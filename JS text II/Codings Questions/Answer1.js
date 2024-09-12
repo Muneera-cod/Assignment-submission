dee707154680d6eff5e1eeed3fa7908530f63459
Muneera <117623918+Muneera-cod@users.noreply.github.com>
2024-09-12 15:51:36 +0530

    Capital letter vowel also added

--- JS text II/Codings Questions/Answer1.js
+++ JS text II/Codings Questions/Answer1.js
@@ -1,7 +1,7 @@
 const countVowel=(string)=>{
     let count=0;
     for(let i=0;i<string.length;i++){
-        if(['a','e','i','o','u'].includes(string[i])){
+        if(['a','e','i','o','u','A','E','I','O','U'].includes(string[i])){
             count=count+1
         }
        
